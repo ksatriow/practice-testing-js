72c438645ce15fe55ca182b03dcd5d58777e8d72
ksatriow <andkukuh@gmail.com>
2021-09-28 06:13:47 +0700

    implement seach feature

--- selenium-cucumber-js/features/step/step.js
+++ selenium-cucumber-js/features/step/step.js
@@ -1,16 +1,19 @@
 const { When, Then, After, Before, Given } = require("@cucumber/cucumber");
 const { expect } = require("chai");
+const webdriver = require("selenium-webdriver");
+const {By} = require("selenium-webdriver");
 
+let driver;
 let multiply = 0;
 
 // Synchronous
 Before(function () {
-  console.log("Didalam scope before");
+  driver = new webdriver.Builder().forBrowser("firefox").build();
 });
 
 // Asynchronous Promise
 After(function () {
-  console.log("Didalam scope after");
+  driver.quit();
 });
 
 When("I multiply {int} and {int}", function (number1, number2) {
@@ -21,10 +24,15 @@ Then("the result should be {int}", function (result) {
   expect(multiply).equal(result);
 });
 
-// Given("I visit github homepage", async () => {
-//     await driver.get("https://github.com/");
-// });
+Given("I visit github homepage", async () => {
+    await driver.get("https://google.com/");
+});
 
-// When("I search ksatriow on github", async () => {});
+When("I search ksatriow on github", {timeout: 60 * 1000}, async () => {
+  await driver.findElement(By.name('q')).sendKeys('ksatriow' + '\n');
+});
 
-// Then("I should see the result", async () => {});
+Then("I should see the result", async () => {
+  let result = await driver.findElement(By.id('search')).getText();
+  console.log(result);
+});
