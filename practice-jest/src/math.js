aefc6a6e2908f3accad88de5146f72b42c8535b8
ksatriow <andkukuh@gmail.com>
2021-10-01 23:04:51 +0700

    add math operator function

---
+++ practice-jest/src/math.js
@@ -0,0 +1,15 @@
+const mathOperations = {
+  add: function (a, b) {
+    return a + b;
+  },
+  min: function (a, b) {
+    return a - b;
+  },
+  mul: function (a, b) {
+    return a * b;
+  },
+  div: function (a, b) {
+    return a / b;
+  },
+};
+module.exports = mathOperations;
