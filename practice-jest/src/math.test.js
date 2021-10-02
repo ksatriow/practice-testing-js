d7da8f49f4fd63e6c59e376f2c56f04120e89eed
ksatriow <andkukuh@gmail.com>
2021-10-02 07:46:30 +0700

    practice test equality matcher, even odd and rolldice

--- practice-jest/src/math.test.js
+++ practice-jest/src/math.test.js
@@ -29,4 +29,47 @@ describe("Math Test", () => {
     // assert
     expect(result).toBe(2);
   });
+
+  // equality matcher
+  test("equality matchers", () => {
+    expect(2 * 2).toBe(4);
+    expect(4 - 2).not.toBe(1);
+  });
+
+  // even odd
+  expect.extend({
+    toBeDivisibleBy(received, argument) {
+      const pass = received % argument == 0;
+      if (pass) {
+        return {
+          pass: true,
+          message: () =>
+            `expected ${received} not to be divisible by ${argument}`,
+        };
+      } else {
+        return {
+          pass: false,
+          message: () => `expected ${received} to be divisible by ${argument}`,
+        };
+      }
+    },
+  });
+
+  test("even and odd numbers", () => {
+    expect(100).toBeDivisibleBy(2);
+    expect(101).not.toBeDivisibleBy(2);
+  });
+
+  // Rolls n virtual dice
+  function rollDice(n) {
+    let answer = [];
+    for (let i = 0; i < n; i++) {
+      answer.push(Math.floor(Math.random() * 6 + 1));
+    }
+    return answer;
+  }
+
+  test("rollDice only returns valid numbers", () => {
+    expect(rollDice(100)).toEqual(expect.arrayContaining([1, 2, 3, 4, 5, 6]));
+  });
 });
